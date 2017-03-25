4ccdec1a278c6277fa197fe28ceb769ab5c04096
Mark Solters <msolters@driblet.io>
2017-03-25 17:56:58 -0400

    setup now uses Driblet's buildroot instead of NextThingCo

--- setup_ubuntu1404.sh
+++ setup_ubuntu1404.sh
@@ -89,7 +89,7 @@ git clone https://github.com/NextThingCo/CHIP-tools.git
 
 echo -e "\n Installing CHIP-buildroot"
 if [ ! -d CHIP-buildroot ]; then
-  git clone http://github.com/NextThingCo/CHIP-buildroot
+  git clone http://github.com/driblet/CHIP-buildroot
 else
   pushd CHIP-buildroot
   git pull
